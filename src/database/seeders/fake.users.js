a1be3363256540f522a9fb1afe6d4753d11cb3a9
almela09 <145797671+almela09@users.noreply.github.com>
2024-05-12 21:50:54 +0200

    feature: faker & faker users seed

---
+++ src/database/seeders/fake.users.js
@@ -0,0 +1,22 @@
+import faker from 'faker';
+import User from '../models/User.js';
+
+const createFakeUser = async () => {
+  const user = new User({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    biography: faker.lorem.sentence(),
+    avatar: faker.image.avatar(),
+  });
+
+  await user.save();
+};
+
+const createFakeUsers = async () => {
+  for (let i = 0; i < 10; i++) {
+    await createFakeUser();
+  }
+};
+
+createFakeUsers();
