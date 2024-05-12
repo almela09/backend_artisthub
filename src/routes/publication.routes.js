f3d977194f3c040588bf3bc3adeb45ffcd8358c8
almela09 <145797671+almela09@users.noreply.github.com>
2024-05-12 21:37:03 +0200

    import publication controller

--- src/routes/publication.routes.js
+++ src/routes/publication.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth.middleware.js";
-
+import { createPublication, updatePublication,deletePublication } from "../controllers/publication.controller.js";
 const router= Router();
 router.post('/', auth, createPublication);
 router.put('/:id', auth, updatePublication);
