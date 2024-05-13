6fc742e0ad5de92563c6b0d6471e38d3b5a0c54d
almela09 <145797671+almela09@users.noreply.github.com>
2024-05-13 10:11:36 +0200

    update post creationPublication with multer middleware

--- src/controllers/publication.controller.js
+++ src/controllers/publication.controller.js
@@ -3,50 +3,43 @@ import Publication from '../models/Publication.js';
 import cloudinary from 'cloudinary';
 
 export const createPublication = async (req, res) => {
-  // Usar Multer para manejar la carga de la imagen
-  upload.single('image')(req, res, async function (err) {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-
-    // Si hay un archivo en la solicitud, sube a Cloudinary
-    let imageUrl = '';
-    if (req.file) {
-      try {
-        const result = await cloudinary.uploader.upload(req.file.path);
-        imageUrl = result.secure_url;
-      } catch (uploadError) {
-        return res.status(500).json({ error: 'Error uploading image to Cloudinary', details: uploadError.message });
-      }
-    }
-
-    // Datos de publicación a crear
-    const { title, text } = req.body;
-    const userId = req.tokenData.userId;  // Asume que userId está en tokenData
-
-    const newPublication = new Publication({
-      user: userId,
-      title: title || '',  // Proporciona un título vacío si no se proporciona
-      text,
-      image: imageUrl,
-      publishedAt: Date.now(),
-      like: []
-    });
-
+  // Si hay un archivo en la solicitud, sube a Cloudinary
+  let imageUrl = '';
+  if (req.file) {
     try {
-      // Guardar la nueva publicación en la base de datos
-      const savedPublication = await newPublication.save();
-      res.status(201).json({
-        message: 'Publication created successfully',
-        publication: savedPublication
-      });
-    } catch (dbError) {
-      res.status(500).json({
-        message: 'Error creating publication',
-        error: dbError.message
-      });
+      const result = await cloudinary.uploader.upload(req.file.path);
+      imageUrl = result.secure_url;
+    } catch (uploadError) {
+      return res.status(500).json({ error: 'Error uploading image to Cloudinary', details: uploadError.message });
     }
+  }
+
+  // Datos de publicación a crear
+  const { title, text } = req.body;
+  const userId = req.tokenData.userId;  // Asume que userId está en tokenData
+
+  const newPublication = new Publication({
+    user: userId,
+    title: title || '',  // Proporciona un título vacío si no se proporciona
+    text,
+    image: imageUrl,
+    publishedAt: Date.now(),
+    like: []
   });
+
+  try {
+    // Guardar la nueva publicación en la base de datos
+    const savedPublication = await newPublication.save();
+    res.status(201).json({
+      message: 'Publication created successfully',
+      publication: savedPublication
+    });
+  } catch (dbError) {
+    res.status(500).json({
+      message: 'Error creating publication',
+      error: dbError.message
+    });
+  }
 };
 export const updatePublication = async (req, res) => {
   // Obtener la ID de la publicación y los datos a actualizar
--- src/routes/publication.routes.js
+++ src/routes/publication.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { auth } from "../middleware/auth.middleware.js";
 import multer from 'multer';
 import { createPublication, updatePublication,deletePublication } from "../controllers/publication.controller.js";
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 const router= Router();
-router.post('/', auth, createPublication);
+router.post('/', auth, upload.single('image'), createPublication);
 router.put('/:id', auth, upload.single('image'), updatePublication);
 router.delete('/:id', auth, deletePublication);
 
