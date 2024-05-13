b082dcbeb4cfb333d52072aa287a21ea257e0147
almela09 <145797671+almela09@users.noreply.github.com>
2024-05-13 10:05:46 +0200

    feature: adding route upload.single and storage

--- src/controllers/publication.controller.js
+++ src/controllers/publication.controller.js
@@ -49,56 +49,49 @@ export const createPublication = async (req, res) => {
   });
 };
 export const updatePublication = async (req, res) => {
-  // Usar Multer para manejar la carga de la imagen
-  upload.single('image')(req, res, async function (err) {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
+  // Obtener la ID de la publicación y los datos a actualizar
+  const publicationId = req.params.id;
+  const { title, text } = req.body;
 
-    // Obtener la ID de la publicación y los datos a actualizar
-    const publicationId = req.params.id;
-    const { title, text } = req.body;
-
-    // Si hay un archivo en la solicitud, sube a Cloudinary
-    let imageUrl;
-    if (req.file) {
-      try {
-        const result = await cloudinary.uploader.upload(req.file.path);
-        imageUrl = result.secure_url;
-      } catch (uploadError) {
-        return res.status(500).json({ error: 'Error uploading image to Cloudinary', details: uploadError.message });
-      }
-    }
-
-    // Construir los datos de actualización
-    const updateData = {
-      title,
-      text
-    };
-
-    // Si se subió una nueva imagen, agregar la URL de la imagen a los datos de actualización
-    if (imageUrl) {
-      updateData.image = imageUrl;
-    }
-
+  // Si hay un archivo en la solicitud, sube a Cloudinary
+  let imageUrl;
+  if (req.file) {
     try {
-      // Actualizar la publicación en la base de datos
-      const updatedPublication = await Publication.findByIdAndUpdate(publicationId, updateData, { new: true, runValidators: true });
-      if (!updatedPublication) {
-        return res.status(404).json({ message: 'Publication not found' });
-      }
-
-      res.status(200).json({
-        message: 'Publication updated successfully',
-        publication: updatedPublication
-      });
-    } catch (dbError) {
-      res.status(500).json({
-        message: "Update publication failed",
-        error: dbError.message
-      });
+      const result = await cloudinary.uploader.upload(req.file.path);
+      imageUrl = result.secure_url;
+    } catch (uploadError) {
+      return res.status(500).json({ error: 'Error uploading image to Cloudinary', details: uploadError.message });
     }
-  });
+  }
+
+  // Construir los datos de actualización
+  const updateData = {
+    title,
+    text
+  };
+
+  // Si se subió una nueva imagen, agregar la URL de la imagen a los datos de actualización
+  if (imageUrl) {
+    updateData.image = imageUrl;
+  }
+
+  try {
+    // Actualizar la publicación en la base de datos
+    const updatedPublication = await Publication.findByIdAndUpdate(publicationId, updateData, { new: true, runValidators: true });
+    if (!updatedPublication) {
+      return res.status(404).json({ message: 'Publication not found' });
+    }
+
+    res.status(200).json({
+      message: 'Publication updated successfully',
+      publication: updatedPublication
+    });
+  } catch (dbError) {
+    res.status(500).json({
+      message: "Update publication failed",
+      error: dbError.message
+    });
+  }
 };
 
 export const deletePublication = async (req, res) => {
--- src/routes/publication.routes.js
+++ src/routes/publication.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth.middleware.js";
+import multer from 'multer';
 import { createPublication, updatePublication,deletePublication } from "../controllers/publication.controller.js";
+const upload = multer({ storage: multer.memoryStorage() });
+
 const router= Router();
 router.post('/', auth, createPublication);
-router.put('/:id', auth, updatePublication);
+router.put('/:id', auth, upload.single('image'), updatePublication);
 router.delete('/:id', auth, deletePublication);
 
 export default router; 
