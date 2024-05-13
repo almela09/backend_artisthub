98a2da2266fb7d8ea7b5b2eedf42284359609ad4
almela09 <145797671+almela09@users.noreply.github.com>
2024-05-13 11:43:50 +0200

    upload images works well POR FIN :')

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,24 +1,47 @@
-import multer from 'multer';
-import cloudinary from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import dotenv from 'dotenv';
-
+// import multer from 'multer';
+// import cloudinary from 'cloudinary';
+// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+// import dotenv from 'dotenv';
+// import path from 'path';
 //config multer y cloudinary, datos en .env
 
+// dotenv.config();
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// });
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary.v2,
+//   params: {
+//       folder: 'imgfolder',
+//       public_id: (req, file) => path.parse(file.originalname).name + '-' + Date.now(),
+//   },
+// });
+
+// export const upload = multer({ storage: storage });
+
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from 'dotenv';
+import path from 'path';
+
 dotenv.config();
-cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
 
-  const storage = new CloudinaryStorage({
-    cloudinary: cloudinary.v2,
-    params: {
-      folder: 'some_folder_name',
-      format: async (req, file) => 'png',
-      public_id: (req, file) => 'computed-filename-using-request',
-    },
-  }); 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-  export const upload = multer({ storage: storage });
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'imgfolder',
+    public_id: (req, file) => path.parse(file.originalname).name + '-' + Date.now(),
+  },
+});
+
+export const upload = multer({ storage: storage });
--- src/routes/publication.routes.js
+++ src/routes/publication.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth.middleware.js";
-import multer from 'multer';
+import { upload } from '../config/multerConfig.js';
+// import multer from 'multer';
 import { createPublication, updatePublication,deletePublication } from "../controllers/publication.controller.js";
 
-const upload = multer({ storage: multer.memoryStorage() });
+// const upload = multer({ storage: multer.memoryStorage() });
 
 const router= Router();
 router.post('/', auth, upload.single('image'), createPublication);
